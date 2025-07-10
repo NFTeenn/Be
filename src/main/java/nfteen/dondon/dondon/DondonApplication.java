dcbf705f65a9d12f9043f1ac19d8038d759dd19d
Jeongyeon_Song <songj239587@gmail.com>
2025-07-10 11:15:35 +0900

    :: project setting

---
+++ src/main/java/nfteen/dondon/dondon/DondonApplication.java
@@ -0,0 +1,11 @@
+package nfteen.dondon.dondon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DondonApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DondonApplication.class, args);
+	}
+}
---
+++ src/test/java/nfteen/dondon/dondon/DondonApplicationTests.java
@@ -0,0 +1,13 @@
+package nfteen.dondon.dondon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DondonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
