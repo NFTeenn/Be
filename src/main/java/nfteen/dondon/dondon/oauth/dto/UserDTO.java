b3a61e8fd07009091444d86736a368d787779988
se0hu1 <leesh6541@gmail.com>
2025-09-02 12:03:21 +0900

    add: implement user entity, DTO, and repository

---
+++ src/main/java/nfteen/dondon/dondon/oauth/dto/UserDTO.java
@@ -0,0 +1,13 @@
+package nfteen.dondon.dondon.oauth.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+
+    private String role;
+    private String name;
+    private String username;
+}
