c68d9021c31300e8f94199d2b155fc2cc717ad87
se0hu1 <leesh6541@gmail.com>
2025-09-02 12:02:56 +0900

    add: implement OAuth2 login and JWT issuance handler

---
+++ src/main/java/nfteen/dondon/dondon/oauth/service/CustomOAuth2UserService.java
@@ -0,0 +1,80 @@
+
+
+package nfteen.dondon.dondon.oauth.service;
+
+import nfteen.dondon.dondon.oauth.dto.CustomOAuth2User;
+import nfteen.dondon.dondon.oauth.dto.GoogleResponse;
+import nfteen.dondon.dondon.oauth.dto.OAuth2Response;
+import nfteen.dondon.dondon.oauth.dto.UserDTO;
+import nfteen.dondon.dondon.oauth.entity.UserEntity;
+import nfteen.dondon.dondon.oauth.repository.UserRepository;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final UserRepository userRepository;
+
+    public CustomOAuth2UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        System.out.println(oAuth2User);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        OAuth2Response oAuth2Response = null;
+
+        if (registrationId.equals("google")) {
+
+            oAuth2Response = new GoogleResponse(oAuth2User.getAttributes());
+
+        } else {
+
+            return null;
+        }
+
+        String username = oAuth2Response.getProvider()+" "+oAuth2Response.getProviderId(); //사용자를 특정할 아이디값 만들기
+        UserEntity existData = userRepository.findByUsername(username);
+
+        if (existData == null) {
+            UserEntity userEntity = new UserEntity();
+            userEntity.setUsername(username);
+            userEntity.setEmail(oAuth2Response.getEmail());
+            userEntity.setName(oAuth2Response.getName());
+            userEntity.setRole("ROLE_USER");
+
+            userRepository.save(userEntity);
+
+            UserDTO userDTO = new UserDTO();
+            userDTO.setUsername(username);
+            userDTO.setName(oAuth2Response.getName());
+            userDTO.setRole("ROLE_USER");
+
+            return new CustomOAuth2User(userDTO);
+        }
+        else {
+            existData.setEmail(oAuth2Response.getEmail());
+            existData.setName(oAuth2Response.getName());
+
+            userRepository.save(existData);
+
+            UserDTO userDTO = new UserDTO();
+            userDTO.setUsername(existData.getUsername());
+            userDTO.setName(existData.getName());
+            userDTO.setRole(existData.getRole());
+
+            return new CustomOAuth2User(userDTO);
+        }
+
+    }
+}
+
