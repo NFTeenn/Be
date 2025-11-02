649280321e5df52a3aa97f9aa6b10647047d33f0
se0hu1 <leesh6541@gmail.com>
2025-11-02 18:34:15 +0900

    add : implement JWTUtil for test token generate and validation

---
+++ src/main/java/nfteen/dondon/dondon/jwt/JWTUtil.java
@@ -0,0 +1,45 @@
+package nfteen.dondon.dondon.jwt;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JWTUtil {
+
+    private SecretKey secretKey;
+
+    public JWTUtil(@Value("${spring.jwt.secret}") String secret) {
+        secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+   public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .setSigningKey(secretKey)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+   }
+
+    public String createTestToken(String subject) {
+        Date now = new Date();
+        Date expiry = new Date(now.getTime() + 1000 * 60 * 60); // 1시간 유효
+        return Jwts.builder()
+                .setSubject(subject)
+                .setIssuedAt(now)
+                .setExpiration(expiry)
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
