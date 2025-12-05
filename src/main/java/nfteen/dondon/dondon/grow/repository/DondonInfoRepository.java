10beafaa40239c078ab0fbc57e73c17cea46798c
se0hu1 <leesh6541@gmail.com>
2025-12-05 14:17:08 +0900

    add: repositories for grow module

---
+++ src/main/java/nfteen/dondon/dondon/grow/repository/DondonInfoRepository.java
@@ -0,0 +1,14 @@
+package nfteen.dondon.dondon.grow.repository;
+
+import nfteen.dondon.dondon.grow.entity.DondonInfo;
+import nfteen.dondon.dondon.grow.entity.MyInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DondonInfoRepository extends JpaRepository<DondonInfo, Long> {
+    List<DondonInfo> findByMyInfoAndGraduationDateIsNotNull(MyInfo myInfo);
+
+    Optional<DondonInfo> findTopByMyInfoOrderByGenDesc(MyInfo myInfo);
+}
---
+++ src/main/java/nfteen/dondon/dondon/grow/repository/MyInfoRepository.java
@@ -0,0 +1,12 @@
+package nfteen.dondon.dondon.grow.repository;
+
+import nfteen.dondon.dondon.auth.entity.GoogleUser;
+import nfteen.dondon.dondon.grow.entity.MyInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MyInfoRepository extends JpaRepository<MyInfo, Long> {
+    Optional<MyInfo> findByUser(GoogleUser user);
+
+}
