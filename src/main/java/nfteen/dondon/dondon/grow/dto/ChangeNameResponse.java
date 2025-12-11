7eb6951d4ac3b24764b606623c6f43800fa47d4a
se0hu1 <leesh6541@gmail.com>
2025-12-11 20:05:34 +0900

    add: response DTO for change name functionality

---
+++ src/main/java/nfteen/dondon/dondon/grow/dto/ChangeNameResponse.java
@@ -0,0 +1,10 @@
+package nfteen.dondon.dondon.grow.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ChangeNameResponse {
+    private String nickname;
+}
