e1c433d60361d1fa031ce1e5bbebfbcfc80bc32b
se0hu1 <leesh6541@gmail.com>
2025-12-12 12:06:28 +0900

    modify : UserPrizeRepository.java

--- src/main/java/nfteen/dondon/dondon/grow/repository/UserPrizeRepository.java
+++ src/main/java/nfteen/dondon/dondon/grow/repository/UserPrizeRepository.java
@@ -3,6 +3,8 @@ package nfteen.dondon.dondon.grow.repository;
 import nfteen.dondon.dondon.grow.entity.Prize;
 import nfteen.dondon.dondon.grow.entity.UserPrize;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,4 +13,6 @@ public interface UserPrizeRepository extends JpaRepository<UserPrize, Long> {
     Optional<UserPrize> findByUserIdAndPrizeCode(Long userId, String code);
     List<UserPrize> findByUserId(Long userId);
 
+    @Modifying
+    @Query("UPDATE UserPrize u SET u.achieved = true WHERE u.user.id = :userId AND u.prize.code = :code")
     int updateAchieved(Long userId, String code);}
